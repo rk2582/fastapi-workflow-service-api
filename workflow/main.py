18eb90ffe6b455284589766e44ee1f09d58aaab7
rahulk <ping2rahulk@gmail.com>
2026-03-23 15:16:42 +0530

    add JWT token generation and organize FastAPI endpoints into routers

--- workflow/main.py
+++ workflow/main.py
@@ -1,26 +1,12 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 import models
-from models import Todos
-from database import SessionLocal, engine
-from typing import Annotated
-from sqlalchemy.orm import Session
+from database import  engine
+from routers import auth, todos
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+app.include_router(auth.router)
+app.include_router(todos.router)
 
-def get_db():
-    db= SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-
-@app.get("/")
-async def read_all(db: db_dependency):
-    return db.query(Todos).all()
-    
